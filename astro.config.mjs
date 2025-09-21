ef654c00afaaaa0e1baa5cf47f72bea3f474f73f
Adrián Fernández de la Torre <adri1197@gmail.com>
2025-09-22 00:20:34 +0200

    Tidy up folders

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://adri1197.github.io',
-  base: '/portfolio',
+//   base: '/portfolio',
   output: 'static',
 });
