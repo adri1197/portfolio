19541db29e49d9a96acd91b9330dd9b812308f90
Adrián Fernández de la Torre <adri1197@gmail.com>
2025-09-22 00:43:21 +0200

    Fix site config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://adri1197.github.io',
+  site: 'https://adri1197.github.io/portfolio',
 //   base: '/portfolio',
   output: 'static',
 });
