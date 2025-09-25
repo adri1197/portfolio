66abec57729b89a5d436dcf64453610da8d764f6
Adrian Fernandez De La Torre <adri1197@gmail.com>
2025-09-25 12:06:24 +0200

    Update config and some path refs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://adri1197.github.io/portfolio/',
-  base: '.',
+  base: process.env.NODE_ENV === 'production' ? '/portfolio' : '/',
   output: 'static',
 });
